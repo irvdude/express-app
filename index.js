f130683c844db1460c37afc3afc51a59d33562cd
Irvin <irvdude44@gmail.com>
2024-06-28 10:02:24 -0400

    express app

---
+++ index.js
@@ -0,0 +1,15 @@
+const { readFile } = require("fs").promises;
+const express = require("express");
+
+const app = express();
+
+//static files from public library
+app.use(express.static("public"));
+
+app.get("/", async (req, res) => {
+	res.send(await readFile("./home.html", "utf8"));
+});
+
+app.listen(4000, () =>
+	console.log(`app is available on http://localhost:4000`),
+);
